25ce4a53e8a44cfef9581d62dba939e3a44384ee
Igor Rafael <yggor14rafa@hotmail.com>
2016-06-23 13:55:03 -0300

    Addind some docs

--- pysort/hybrid.py
+++ pysort/hybrid.py
@@ -3,11 +3,22 @@ from insertion import *
 from quick import *
 
 def hybrid_sort(list):
+    """
+    Calculates the maximum depth of recursion based on the number of elements in list.
+    @param list = a list object.
+    """
     l = len(list)
     maxdepth = int(log(l,2)) * int(log(l,2))		
     _hybridsort(list, 0, l - 1, maxdepth)
 
 def _hybridsort(list, first, last, maxdepth):
+    """
+    Performs Hybridsort, beginning with quicksort and switching to insertion sort when maximum depth is reached.
+    @param list = a list object. 
+    @param first = an integer, initial index of list.
+    @param last = an integer, final index of list.
+    @param maxdepth = an integer, initially indicates the maximum level of recursion, and it's decreased at each level. 
+    """
     if maxdepth == 0:
         insertion_sort(list)
 	return
