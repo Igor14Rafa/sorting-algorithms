59743e65caad8fc5b7f8e517ae7228ddd68994ff
Igor Rafael <yggor14rafa@hotmail.com>
2016-06-23 12:38:58 -0300

    Update test.py
    Adding the test to Hybrid sort.

--- test.py
+++ test.py
@@ -63,6 +63,11 @@ class SortAlgs(unittest.TestCase):
         list = SortingList(build_list(30))
         list.selection_sort()
         self.assertTrue(is_sorted(list))
+        
+    def test_hybrid(self):
+        list = SortingList(build_list(30))
+        list.hybrid_sort()
+        self.assertTrue(is_sorted(list))
 
 
 if __name__ == '__main__':
